e22340cd04052e7000d221227744e59f5dfa2a49
Emna Souidi <emna.jlassi80@gmail.com>
2024-02-14 12:28:50 +0100

    Ajoutez des fichiers projet.

---
+++ AngularAuthAPI/Controllers/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Controllers
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options): base(options) 
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users"); 
+        }
+    }
+}
